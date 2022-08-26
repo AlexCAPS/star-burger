82d928e059b10be276f25253815dc0d0f49f15eb
Александр Харук <a.haruk@gmail.com>
2022-08-27 00:54:52 +0300

    dd data validation for register_order.

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,6 +1,9 @@
+from collections import defaultdict
+
 from django.db import transaction
 from django.http import JsonResponse
 from django.templatetags.static import static
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -61,6 +64,11 @@ def product_list_api(request):
 
 @api_view(['POST'])
 def register_order(request):
+    errors = validate_order_body(request.data)
+
+    if errors:
+        return Response(errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
     order = save_user_order(request.data)
     return Response({'order_num': order.pk})
 
@@ -78,3 +86,26 @@ def save_user_order(order_body):
         order.products.create(product_id=product['product'], quantity=product['quantity'])
 
     return order
+
+
+def validate_order_body(order_body: dict):
+    errors = defaultdict(list)
+
+    required_str_keys = ['firstname', 'lastname', 'phonenumber', 'address']
+    for str_key in required_str_keys:
+        try:
+            if not isinstance(order_body[str_key], str):
+                errors[str_key].append(f'{str_key} must be a string')
+        except KeyError as missing_key:
+            errors[str_key].append(f'{missing_key} not found')
+
+    if 'products' not in order_body.keys():
+        errors['products'].append('products not found')
+
+    if not isinstance(order_body.get('products'), list):
+        errors['products'].append('products must be list')
+    else:
+        if not len(order_body['products']):
+            errors['products'] = ['products cannot be empty']
+
+    return errors
